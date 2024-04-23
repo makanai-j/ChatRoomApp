3a41ef8d73bacbb75c66d55c038d866ef8f9e23c
makanai-j <kd1365575@st.kobedenshi.ac.jp>
2024-04-23 14:43:08 +0900

    initial commit

---
+++ src/components/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Comment from "../UiParts/Comment.vue";
+</script>
+<template>
+  <div class="user-list"></div>
+  <div class="chat-space">
+    <Comment></Comment>
+  </div>
+  <div class="section-list"></div>
+</template>
---
+++ src/components/UiParts/Comment.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+let oldTime: number = 1713765070000;
+let newTime: number = 1713765076528;
+
+let disTime: number = Math.floor((newTime - oldTime) / 1000);
+</script>
+
+<template>
+  <div class="top-info">
+    <img class="top-item icon-img" src="../../assets/vue.svg" />
+    <p class="top-item name">user name</p>
+    <p class="top-item time">{{ disTime }} s</p>
+  </div>
+</template>
+
+<style scoped>
+.top-item {
+  padding: 5px;
+}
+.top-info {
+  display: flex;
+}
+</style>
